61c34abe9297db548eee56d3c98dfd13473be4bc
Wilma <wilmareistad@icloud.com>
2026-02-03 14:15:27 +0100

    added a massage if everthing is ok

--- src/output/formatter.js
+++ src/output/formatter.js
@@ -1,5 +1,10 @@
 export function printResults(results, mode = "passive") {
-  results.forEach(r => {
+  if (results.length === 0) {
+    console.log("Congratulations you did good");
+    return;
+  }
+
+  results.forEach((r) => {
     const message = mode === "serious" ? r.seriousMessage : r.passiveMessage;
     console.log(message);
   });
