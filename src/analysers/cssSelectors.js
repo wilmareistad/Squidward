abb5a1cc437f3fba5af4ae60edc4305fbddcfdaf
Wilma <wilmareistad@icloud.com>
2026-02-08 17:44:19 +0100

    added a function checkCssSelectors

---
+++ src/analysers/cssSelectors.js
@@ -0,0 +1,39 @@
+export function checkCssSelectors(code) {
+
+    const selectorRegex = /([^{]+)\{/g;
+
+    let totalSelectors = 0;
+    let classSelectors = 0;
+    let match;
+
+    while ((match = selectorRegex.exec(cssCode)) !== null) {
+    const selectors = match[1]
+      .split(",")
+      .map(s => s.trim());
+
+      selectors.forEach(selector => {
+        totalSelectors++;
+
+        if(selector.startsWith(".")) {
+            classSelectors++;
+        }
+      })
+    }
+    
+    if (totalSelectors === 0) {
+        return results;
+    }
+
+    const ratio = classSelectors / totalSelectors;
+
+    if (ratio >= 0.9) {
+    results.push({
+      type: "css-selectors",
+      totalSelectors,
+      classSelectors,
+      ratio
+    });
+  }
+
+  return results;
+}
